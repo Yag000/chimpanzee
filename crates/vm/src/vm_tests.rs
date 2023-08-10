3f961e3c55e07614e2f9ace58eed2faa0b71b43c
Yago Iglesias <yagoiglesias7@gmail.com>
2023-08-11 00:58:23 +0200

    Add falling tests to diagnose the bug

--- crates/vm/src/vm_tests.rs
+++ crates/vm/src/vm_tests.rs
@@ -942,4 +942,200 @@ mod tests {
 
         run_vm_tests(tests);
     }
+    #[test]
+    fn test_use_same_varaible_multiple_times() {
+        let tests = vec![
+            VmTestCase {
+                input: r#"
+                    let a = 1;
+                    let b = a;
+                    let c = a + b + 1;
+                    c"#
+                .to_string(),
+                expected: Object::INTEGER(3),
+            },
+            VmTestCase {
+                input: r#"
+                    let a = 1;
+                    let b = a;
+                    let c = a + b + 1;
+                    let a = 2;
+                    let b = a;
+                    let c = a + b + 1;
+                    c"#
+                .to_string(),
+                expected: Object::INTEGER(5),
+            },
+            VmTestCase {
+                input: r#"
+                    let a = "hello";
+                    let b = "world";
+                    let c = a + b;
+                    let d = a + b + c;
+                    d"#
+                .to_string(),
+                expected: Object::STRING("helloworldhelloworld".to_string()),
+            },
+        ];
+
+        run_vm_tests(tests);
+    }
+
+    #[test]
+    fn test_multiple_use_same_variable_same_line_() {
+        let tests = vec![
+            VmTestCase {
+                input: r#"
+                    let a = 1;
+                    let b = a * a + 2
+                    b"#
+                .to_string(),
+                expected: Object::INTEGER(3),
+            },
+            VmTestCase {
+                input: r#"
+                    let a = 1;
+                    let a = a + 1;
+                    a"#
+                .to_string(),
+                expected: Object::INTEGER(2),
+            },
+            VmTestCase {
+                input: r#"
+                   let a = 1;
+                   let b = a + 1;
+                   let a = a + b;
+                   a"#
+                .to_string(),
+                expected: Object::INTEGER(3),
+            },
+            VmTestCase {
+                input: r#"
+                       let a = "hello";
+                       let a = a + "world";
+                       let a = a + "world";
+                       a"#
+                .to_string(),
+                expected: Object::STRING("helloworldworld".to_string()),
+            },
+        ];
+
+        run_vm_tests(tests);
+    }
+
+    #[test]
+    fn test_array_multiple_ussage() {
+        let tests = vec![
+            VmTestCase {
+                input: r#"
+                let array = [1,2,3];
+                let value =  array[1] + array[2];
+                value"#
+                    .to_string(),
+                expected: Object::INTEGER(5),
+            },
+            VmTestCase {
+                input: r#"
+                let array = [1,2,3];
+                let array = push(array, 4);
+                array"#
+                    .to_string(),
+                expected: Object::ARRAY(vec![
+                    Object::INTEGER(1),
+                    Object::INTEGER(2),
+                    Object::INTEGER(3),
+                    Object::INTEGER(4),
+                ]),
+            },
+        ];
+        run_vm_tests(tests);
+    }
+
+    #[test]
+    fn test_shadowing_same_type() {
+        let tests = vec![
+            VmTestCase {
+                input: r#"
+                    let x = 4;
+                    let x = 5;
+                    x"#
+                .to_string(),
+                expected: Object::INTEGER(5),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = [1,2,3];
+                    let x = [4,5,6];
+                    x"#
+                .to_string(),
+                expected: Object::ARRAY(vec![
+                    Object::INTEGER(4),
+                    Object::INTEGER(5),
+                    Object::INTEGER(6),
+                ]),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = fn() { 1 };
+                    let x = fn() { 2 };
+                    x()"#
+                    .to_string(),
+                expected: Object::INTEGER(2),
+            },
+        ];
+
+        run_vm_tests(tests);
+    }
+
+    #[test]
+    fn test_shadowing_with_new_type() {
+        let tests = vec![
+            VmTestCase {
+                input: r#"
+                    let x = 4;
+                    let x = "string";
+                    x"#
+                .to_string(),
+                expected: Object::STRING("string".to_string()),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = "string";
+                    let x = fn() { 1 };
+                    x()"#
+                    .to_string(),
+                expected: Object::INTEGER(1),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = fn() { 1 };
+                    let x = 5,
+                    x"#
+                .to_string(),
+                expected: Object::INTEGER(5),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = 5;
+                    let x = [1,2,3];
+                    x"#
+                .to_string(),
+                expected: Object::ARRAY(vec![
+                    Object::INTEGER(1),
+                    Object::INTEGER(2),
+                    Object::INTEGER(3),
+                ]),
+            },
+            VmTestCase {
+                input: r#"
+                    let x = [1,2,3];
+                    let x = 5;
+                    x"#
+                .to_string(),
+                expected: Object::INTEGER(5),
+            },
+        ];
+
+        run_vm_tests(tests);
+    }
 }
